495223534487b48879ca904ed6ce3dba37390d6b
Masahiro Aikawa <aikawame@wor.jp>
2019-03-24 23:45:40 +0900

    Twitterに対応する

--- content.js
+++ content.js
@@ -1,9 +1,17 @@
 import keywordManager from './keyword_manager.js'
 import ShindanMarker from './shindan_marker.js'
 
-keywordManager.repeatUpdating()
-let shindanMarker = new ShindanMarker(
-  keywordManager,
-  "a[data-testid='trendLink']"
-)
-shindanMarker.execute()
+let targetSelector = null
+switch (window.location.hostname) {
+  case 'twitter.com':
+    targetSelector = "div[data-testid='trend'] > div:nth-child(2) > span"
+    break
+  case 'tweetdeck.twitter.com':
+    targetSelector = "a[data-testid='trendLink']"
+    break
+}
+if (targetSelector) {
+  keywordManager.repeatUpdating()
+  let shindanMarker = new ShindanMarker(keywordManager, targetSelector)
+  shindanMarker.execute()
+}
